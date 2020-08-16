6cf08bc71fa5e4d9ef1af8d23803286150fd1a28
Usama Subhani <reachusama@protonmail.com>
2020-08-16 16:32:16 +0500

    Add Launch component and Apollo Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks'; 
 import './App.css';
+import Launches from './components/Launches';
+
+const client = new ApolloClient({
+  uri: 'http://api.spacex.land/graphql/',
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>SpaceX</h1>
+        <Launches />
+      </div>
+      </ApolloProvider>
   );
 }
 

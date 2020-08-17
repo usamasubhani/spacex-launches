342357f5a06407c64da28a0ffe2a26d06bd2fa54
Usama Subhani <reachusama@protonmail.com>
2020-08-17 20:35:14 +0500

    Add home button

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,20 +1,16 @@
 import React from 'react'
-import { AppBar, Typography, IconButton, Toolbar } from '@material-ui/core'
+import { AppBar, Typography, IconButton, Toolbar, Button } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
     return (
         <>
-           <AppBar position="static">
-            <Toolbar>
-                <IconButton edge="start"  color="inherit" aria-label="menu">
-                <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" >
-                SpaceX
-                </Typography>
-            </Toolbar>
-            </AppBar>  
+            <AppBar position="static">
+                <Toolbar>
+                    <Button component={Link} to="/" color="inherit">SpaceX</Button>
+                </Toolbar>
+            </AppBar>
         </>
     )
 }
